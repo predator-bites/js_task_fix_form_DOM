5ba2e3249c749e9c1430916efc1a1b8ed6de315f
DropPredator <ionchiccc@gmail.com>
2025-09-03 09:32:11 +0300

    solved task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+function getInputs() {
+  return [...document.querySelectorAll('input')];
+}
+
+function createLabels(list) {
+  list.forEach((elem) => {
+    const innerString = String(elem.name).toUpperCase();
+    const label = document.createElement('label');
+    const placeHolderText = innerString.toLowerCase().split('');
+
+    placeHolderText[0] = placeHolderText[0].toUpperCase();
+
+    elem.insertAdjacentElement('beforebegin', label);
+    elem.setAttribute('placeholder', placeHolderText.join(''));
+
+    label.setAttribute('for', elem.id);
+    label.setAttribute('style', 'font-weight: bold');
+    label.innerText = innerString;
+    label.classList.add('field-label');
+  });
+}
+
+createLabels(getInputs());
