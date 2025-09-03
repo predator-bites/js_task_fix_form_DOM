05d865eaf19416e6eefbcc3c13d64d812375507f
DropPredator <ionchiccc@gmail.com>
2025-09-03 10:18:02 +0300

    changes implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,41 @@
 'use strict';
 
 function getInputs() {
-  return [...document.querySelectorAll('input')];
+  return [...document.querySelectorAll('form input')];
 }
 
 function createLabels(list) {
-  list.forEach((elem) => {
-    const innerString = String(elem.name).toUpperCase();
-    const label = document.createElement('label');
-    const placeHolderText = innerString.toLowerCase().split('');
+  const usedIds = [];
 
-    placeHolderText[0] = placeHolderText[0].toUpperCase();
+  list.forEach((input) => {
+    if (!usedIds.includes(input.id)) {
+      const label = document.createElement('label');
+      const placeHolderText = input.name.toLowerCase().split('');
 
-    elem.insertAdjacentElement('beforebegin', label);
-    elem.setAttribute('placeholder', placeHolderText.join(''));
+      placeHolderText[0] = placeHolderText[0].toUpperCase();
 
-    label.setAttribute('for', elem.id);
-    label.setAttribute('style', 'font-weight: bold');
-    label.innerText = innerString;
-    label.classList.add('field-label');
+      if (input.id === '') {
+        while (true) {
+          const randomNum = Math.floor(Math.random() * 1000000000).toString();
+
+          if (!document.getElementById(randomNum)) {
+            input.id = randomNum;
+
+            break;
+          }
+        }
+      }
+
+      input.parentNode.appendChild(label);
+
+      input.setAttribute('placeholder', placeHolderText.join(''));
+
+      label.setAttribute('for', input.id);
+      label.innerText = input.name;
+      label.classList.add('field-label');
+
+      usedIds.push(input.id);
+    }
   });
 }
 
